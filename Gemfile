cd310c618b2371297ce4256155550602e516f87a
Langton <langtonfavor@gmail.com.com>
2021-11-10 18:48:56 +0200

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem 'rails-controller-testing'
 
 gem 'bcrypt'
 
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :name,  presence: true, length:  { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
