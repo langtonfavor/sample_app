d6408751faa067c2bcaeadf8debcc5fa44ca59fc
Langton <langtonfavor@gmail.com.com>
2021-11-10 10:26:09 +0200

    sorted the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'static_pages/contact'
   get 'signup'  => 'users#new'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
