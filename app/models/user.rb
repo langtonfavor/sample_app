58c9a938c74bed7b0ac12036be02816243535592
Langton <langtonfavor@gmail.com.com>
2021-11-11 13:00:07 +0200

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
 
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: false
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :name,  presence: true, length:  { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-
+  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
   has_secure_password
 
   has_many :microposts
