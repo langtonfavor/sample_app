0b2e0b983d5d4b797a1f4cce66de1a2c5e6df3ee
Langton <langtonfavor@gmail.com.com>
2021-11-11 13:00:07 +0200

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,34 +23,6 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
-
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
-
-  # Sends password reset email.
-  def send_password_reset_email
-    UserMailer
-  end
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
-
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
-
-  # Sends password reset email.
-  def send_password_reset_email
-    UserMailer
-  end
 
   private
 
