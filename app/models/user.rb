0ace85d9743da8c6cbd6f60e64d448696821ba01
Langton <langtonfavor@gmail.com.com>
2021-11-11 15:13:34 +0200

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,10 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
