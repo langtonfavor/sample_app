c3b5fb9bcc55147af66136273f3d76dacb282966
Langton <langtonfavor@gmail.com.com>
2021-11-12 11:20:44 +0200

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :correct_user, only: [:edit, :update, :destroy, :show]
-  before_action :admin_user, only: [:destroy]
 
   def index
     @user = User.paginate(page: params[:page])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,40 @@
 class User < ApplicationRecord
 
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: false
-  validates :name,  presence: true, length:  { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
   has_secure_password
+
   has_many :microposts
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+           foreign_key: "follower_id",
+           dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+           foreign_key: "followed_id",
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
-  before_save   :downcase_email
+  before_save :downcase_email
   before_save { self.email = email.downcase }
 
   attr_accessor :remember_token, :activation_token
   attr_accessor :remember_token, :activation_token, :reset_token
 
-  default_scope -> { order(created_at: :desc)}
+  default_scope -> { order(created_at: :desc) }
 
   def activate
-    update_attribute(:activated,    true)
+    update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
   end
 
-  def feed
-    Micropost.where("user_id = ?", id)
-  end
-
+  Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+                  following_ids: following_ids, user_id: id)
 
   # Sends activation email.
   def send_activation_email
@@ -40,13 +47,28 @@ class User < ApplicationRecord
 
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
   # Sends password reset email.
   def send_password_reset_email
     UserMailer
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
@@ -57,7 +79,7 @@ class User < ApplicationRecord
   end
 
   def create_activation_digest
-    self.activation_token  = User.new_token
+    self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
 
